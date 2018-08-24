d411a2482725890c41f7db2abdd1f896523feb5f
edmorado <elkingmoradoking@gmail.com>
2018-08-24 17:05:19 +0800

    Updated from index to string search for fields

--- prokkaTabler.py
+++ prokkaTabler.py
@@ -2,23 +2,25 @@
 
 import glob
 
-files = glob.glob('*.tsv')
-outfile = open("quast.summary","w")
-outfile.write("sample\t#contigs>=16bp\ttotalLength>=16bp\t#contigs\tlargestContg\ttotalLength\t%GC\tN50\tN75\tL50\tL75\t#NsPer100kbp\n")
+files = glob.glob('*.txt')
+outfile = open("prokka.summary","w")
+outfile.write("sample\tcontigs\tbases\tCDS\trepeat_region\ttRNA\ttmRNA\n")
 
 for element in files:
+	sample = element.replace(".txt","")
 	tsv = open(element,"r").read().split("\n")
-	sample = tsv[0].split("\t")[2]
-        numContigsG16 = tsv[1].split("\t")[2]
-        totalLenG16 = tsv[2].split("\t")[2]
-        numContigs = tsv[3].split("\t")[2]
-        largestCont = tsv[4].split("\t")[2]
-        totalLen = tsv[5].split("\t")[2]
-        GC = tsv[6].split("\t")[2]
-        N50 = tsv[7].split("\t")[2]
-        N75 = tsv[8].split("\t")[2]
-        L50 = tsv[9].split("\t")[2]
-        L75 = tsv[10].split("\t")[2]
-        Np100 = tsv[11].split("\t")[2]
-	outfile.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\t{10}\t{11}\n".format(sample,numContigsG16,totalLenG16,numContigs,largestCont,totalLen,GC,N50,N75,L50,L75,Np100))
+	for line in tsv:
+		if "contigs:" in line:
+			contigs = line.split(" ")[1]
+                elif "bases:" in line:
+		        bases = line.split(" ")[1]
+                elif "CDS:" in line:
+		        CDS = line.split(" ")[1]
+                elif "repeat_region:" in line:
+		        repeat_region = line.split(" ")[1]
+                elif "tRNA:" in line:
+		        tRNA = line.split(" ")[1]
+                elif "tmRNA:" in line:
+		        tmRNA = line.split(" ")[1]
+	outfile.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\n".format(sample,contigs,bases,CDS,repeat_region,tRNA,tmRNA,))
 outfile.close()		
